e0602ce9a6a8f99945334bd5823fc83f2c468091
nozomitaguchi <nozomi.taguchi@gmail.com>
2020-01-03 23:44:30 +0900

    Refactor

--- src/main/scala/com/creatorships/web/comic/catalogs/domain/analyze/rule/AnalyzeRuleType.scala
+++ src/main/scala/com/creatorships/web/comic/catalogs/domain/analyze/rule/AnalyzeRuleType.scala
@@ -1,20 +1,18 @@
 package com.creatorships.web.comic.catalogs.domain.analyze.rule
 
+import com.creatorships.web.comic.catalogs.domain.analyze.rule.AnalyzeRuleType.Publisher
+
 sealed abstract class AnalyzeRuleType(override val toString: String) {
 
-  def isPublisher: Boolean
+  def isPublisher: Boolean = toString == Publisher.toString
 
 }
 
 object AnalyzeRuleType {
 
-  case object Publisher extends AnalyzeRuleType("publisher") {
-    override def isPublisher: Boolean = true
-  }
+  case object Publisher extends AnalyzeRuleType("publisher")
 
-  case object Comic extends AnalyzeRuleType("comic") {
-    override def isPublisher: Boolean = false
-  }
+  case object Comic extends AnalyzeRuleType("comic")
 
   private val typeByName: Map[String, AnalyzeRuleType] = Seq(Publisher, Comic).map(t => t.toString -> t).toMap
 
